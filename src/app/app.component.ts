8c52e73e447ebb27088a54c788d6599faf13a377
Lucas Job <ljba@cin.ufpe.br>
2021-12-17 00:13:22 -0300

    adicionando firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { firestore } from '../firebase';
+import { collection, addDoc } from "firebase/firestore"; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cardapio';
+
+  async firebase_test(){
+    try {
+      const docRef = await addDoc(collection(firestore, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    console.log(firestore)
+  }
 }
